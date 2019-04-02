a920277fb0810bb4f7c2ff73dc1076d579fb8897
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-04-01 20:02:34 -0600

    Add Student .log_score and .grade method tests

--- test/student_test.rb
+++ test/student_test.rb
@@ -18,4 +18,18 @@ class StudentTest < Minitest::Test
   def test_it_starts_with_no_scores
     assert_equal [], @morgan.scores
   end
+
+  def test_it_can_log_multiple_scores
+    @morgan.log_score(89)
+    @morgan.log_score(78)
+
+    assert_equal [89, 78], @morgan.scores
+  end
+
+  def test_it_can_return_an_average_of_all_scores
+    @morgan.log_score(89)
+    @morgan.log_score(78)
+
+    assert_equal 83.5, @morgan.grade
+  end
 end
